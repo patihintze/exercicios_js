94393af3c79ac7c7461cea0da6c58cec01550076
patihintze <patihintze@outlook.com>
2024-04-19 21:09:59 -0300

    subindo ex2

---
+++ ex2_triplets.js
@@ -0,0 +1,12 @@
+var a = [2,2,3];
+var b = [1,2,1]
+
+var alice = 0;
+var bob = 0;
+for (var i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) {
+        alice++;
+    } else if (a[i] < b[i]){
+        bob++;
+    }
+}
